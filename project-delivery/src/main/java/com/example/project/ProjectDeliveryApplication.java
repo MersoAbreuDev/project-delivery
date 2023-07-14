639f9e3368c860fcd36c77ad1542db7372d300b1
DevMersoAbreu <emerson.solutions@outlook.com>
2023-07-13 21:51:22 -0300

    inicio projeto

---
+++ project-delivery/src/main/java/com/example/project/ProjectDeliveryApplication.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+public class ProjectDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDeliveryApplication.class, args);
+	}
+
+}
